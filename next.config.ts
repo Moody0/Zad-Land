4486218758215143f8c8195a75ba05d2d4bc655e
Moody0 <moody0@users.noreply.github.com>
2026-08-31 13:46:05 +0300

    fix: bypass Vercel image optimizer with unoptimized true

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,7 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ['react-icons'],
   },
   images: {
+    unoptimized: true,
     minimumCacheTTL: 31536000,
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
